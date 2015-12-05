01f9611a6065fc43f9d2c4e63230801c69c7f14e
Daniel Davis <daniel.davis@nih.gov>
2015-12-05 01:09:54 -0500

    Add the packages for groupinstall "Development Tools"

--- occs-ab_gcc/manifests/init.pp
+++ occs-ab_gcc/manifests/init.pp
@@ -1,41 +1,53 @@
 # == Class: ab_gcc
 #
-# Full description of class ab_gcc here.
+# Installs the "Development Tools" group from yum repos.
+# The packages were determined by running 
+#     yum groupinfo "Development Tools"
+# on CentOS 6.
 #
 # === Parameters
 #
-# Document parameters here.
-#
-# [*sample_parameter*]
-#   Explanation of what this parameter affects and what it defaults to.
-#   e.g. "Specify one or more upstream ntp servers as an array."
-#
-# === Variables
-#
-# Here you should define a list of variables that this module would require.
-#
-# [*sample_variable*]
-#   Explanation of how this variable affects the funtion of this class and if
-#   it has a default. e.g. "The parameter enc_ntp_servers must be set by the
-#   External Node Classifier as a comma separated list of hostnames." (Note,
-#   global variables should be avoided in favor of class parameters as
-#   of Puppet 2.6.)
+# None
 #
 # === Examples
 #
-#  class { ab_gcc:
-#    servers => [ 'pool.ntp.org', 'ntp.local.company.com' ],
-#  }
+#  class { ab_gcc: }
 #
 # === Authors
 #
-# Author Name <author@domain.com>
-#
-# === Copyright
-#
-# Copyright 2015 Your name here, unless otherwise noted.
+# Dan Davis <daniel.davis@nih.gov>
 #
 class ab_gcc {
-
-
+  package {['autoconf',
+            'automake',
+            'binutils',
+            'bison',
+            'flex',
+            'gcc',
+            'gcc-c++',
+            'gettext',
+            'libtool',
+            'make',
+            'patch',
+            'pkgconfig',
+            'redhat-rpm-config',
+            'rpm-build',
+            'rpm-sign',
+            'byacc',
+            'cscope',
+            'ctags',
+            'diffstat',
+            'doxygen',
+            'elfutils',
+            'gcc-gfortran',
+            'git',
+            'indent',
+            'intltool',
+            'patchutils',
+            'rcs',
+            'subversion',
+            'swig',
+            'systemtap']:
+    ensure => present
+  }
 }
