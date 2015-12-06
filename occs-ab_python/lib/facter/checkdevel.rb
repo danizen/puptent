0476046be9c9acb61dbf228635da7e47ac3f05b3
Daniel Davis <daniel.davis@nih.gov>
2015-12-05 23:13:02 -0500

    No need for facter check with params pattern and =~

--- occs-ab_python/lib/facter/checkdevel.rb
+++
@@ -1,8 +0,0 @@
-require 'facter'
-
-Facter.add(:ab_python_devel) do
-    setcode do
-        `hostname -s`.start_with?('d') ? true : nil
-    end
-end
-
--- occs-ab_ruby/lib/facter/checkdevel.rb
+++
@@ -1,8 +0,0 @@
-require 'facter'
-
-Facter.add(:ab_ruby_devel) do
-    setcode do
-        `hostname -s`.start_with?('d') ? true : nil
-    end
-end
-
