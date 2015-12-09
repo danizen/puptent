304e34e1741fba4b43a21032f810e9e3b31ed096
Daniel Davis <davisda4@mail.nlm.nih.gov>
2015-12-08 22:26:55 -0500

    Adjust to hopefully work with STB/SSS names.

--- occs-ab_python/lib/facter/checkrepo.rb
+++ occs-ab_python/lib/facter/checkrepo.rb
@@ -3,5 +3,5 @@ require 'facter'
 # NOTE: grep may need to be adjusted to STB/SSS assigned channel name
 Facter.add(:ab_python_have_repo) do
   confine :osfamily => 'RedHat'
-  setcode '/bin/yum -C repolist | /bin/grep -i "python 3\.4" | /bin/wc -l'
+  setcode '/usr/bin/yum repolist 2>/dev/null | /bin/grep -i -E "python 3\.?4" | /usr/bin/wc -l'
 end
--- occs-ab_ruby/lib/facter/checkrepo.rb
+++ occs-ab_ruby/lib/facter/checkrepo.rb
@@ -3,5 +3,5 @@ require 'facter'
 # NOTE: grep may need to be adjusted for STB/SSS assigned channel name
 Facter.add(:ab_ruby_have_repo) do
   confine :osfamily => 'RedHat'
-  setcode '/bin/yum -C repolist | /bin/grep -i "ruby 2\.2" | /bin/wc -l'
+  setcode '/usr/bin/yum repolist 2>/dev/null | /bin/grep -i -E "ruby 2\.?2" | /usr/bin/wc -l'
 end
