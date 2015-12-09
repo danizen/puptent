90f28ec4b1d112c6bcee22f134a39e7da14e0fe6
Daniel Davis <davisda4@mail.nlm.nih.gov>
2015-12-09 00:09:00 -0500

    Change the facter checks based on the new repository structure.

--- occs-ab_python/lib/facter/checkrepo.rb
+++ occs-ab_python/lib/facter/checkrepo.rb
@@ -3,5 +3,5 @@ require 'facter'
 # NOTE: grep may need to be adjusted to STB/SSS assigned channel name
 Facter.add(:ab_python_have_repo) do
   confine :osfamily => 'RedHat'
-  setcode '/usr/bin/yum repolist 2>/dev/null | /bin/grep -i -E "python 3\.?4" | /usr/bin/wc -l'
+  setcode '/usr/bin/yum list rh-python34 2>/dev/null | /bin/grep "^rh-python34" | /usr/bin/wc -l'
 end
--- occs-ab_ruby/lib/facter/checkrepo.rb
+++ occs-ab_ruby/lib/facter/checkrepo.rb
@@ -3,5 +3,5 @@ require 'facter'
 # NOTE: grep may need to be adjusted for STB/SSS assigned channel name
 Facter.add(:ab_ruby_have_repo) do
   confine :osfamily => 'RedHat'
-  setcode '/usr/bin/yum repolist 2>/dev/null | /bin/grep -i -E "ruby 2\.?2" | /usr/bin/wc -l'
+  setcode '/usr/bin/yum list rh-ruby22 2>/dev/null | /bin/grep -i "^rh-ruby22" | /usr/bin/wc -l'
 end
