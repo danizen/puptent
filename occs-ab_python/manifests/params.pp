186b6f43cadfbcd57a0733db6c373d565b86dd83
Daniel Davis <davisda4@mail.nlm.nih.gov>
2015-12-09 13:23:18 -0500

    Change these modules so that $devel defaults to nil.

--- occs-ab_python/manifests/params.pp
+++ occs-ab_python/manifests/params.pp
@@ -1,7 +1,3 @@
 class ab_python::params {
-  if ($hostname =~ /^d/) {
-    $devel = true
-  } else {
-    $devel = nil
-  }
+  $devel = nil
 }
--- occs-ab_ruby/manifests/params.pp
+++ occs-ab_ruby/manifests/params.pp
@@ -1,7 +1,3 @@
 class ab_ruby::params {
-  if ($hostname =~ /^d/) {
-    $devel = true
-  } else {
-    $devel = nil
-  }
+  $devel = nil
 }
